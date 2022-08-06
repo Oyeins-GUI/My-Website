e1a10ee192fd5b960824c1b7e7e19f65da398058
Oyeins-GUI <oyeinx7@gmail.com>
2022-08-06 22:48:38 +0100

    first commit

--- tests/smart-claimant_test.ts
+++ tests/smart-claimant_test.ts
@@ -8,24 +8,43 @@ import {
 import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
 Clarinet.test({
-  name: "Ensure that <...>",
+  name: "Disburses tokens once it can claim the time-locked wallet balance",
   async fn(chain: Chain, accounts: Map<string, Account>) {
-    let block = chain.mineBlock([
-      /*
-       * Add transactions with:
-       * Tx.contractCall(...)
-       */
-    ]);
-    assertEquals(block.receipts.length, 0);
-    assertEquals(block.height, 2);
+    const deployer = accounts.get("deployer")!.address;
+    const beneficiary = `${deployer}.smart-claimant`;
+    const wallet1 = accounts.get("wallet_1")!.address;
+    const wallet2 = accounts.get("wallet_2")!.address;
+    const wallet3 = accounts.get("wallet_3")!.address;
+    const wallet4 = accounts.get("wallet_4")!.address;
+    const unlock_height = 10;
+    const amount = 1000;
+    const share = Math.floor(amount / 4);
 
-    block = chain.mineBlock([
-      /*
-       * Add transactions with:
-       * Tx.contractCall(...)
-       */
+    chain.mineBlock([
+      Tx.contractCall(
+        "timelocked",
+        "lock",
+        [
+          types.principal(beneficiary),
+          types.uint(unlock_height),
+          types.uint(amount),
+        ],
+        deployer
+      ),
     ]);
-    assertEquals(block.receipts.length, 0);
-    assertEquals(block.height, 3);
+
+    chain.mineEmptyBlockUntil(unlock_height);
+
+    let block = chain.mineBlock([
+      Tx.contractCall("smart-claimant", "claim", [], deployer),
+    ]);
+
+    let [receipt] = block.receipts;
+    receipt.result.expectErr().expectUint(104);
+
+    receipt.events.expectSTXTransferEvent(share, beneficiary, wallet1);
+    receipt.events.expectSTXTransferEvent(share, beneficiary, wallet2);
+    receipt.events.expectSTXTransferEvent(share, beneficiary, wallet3);
+    receipt.events.expectSTXTransferEvent(share, beneficiary, wallet4);
   },
 });
